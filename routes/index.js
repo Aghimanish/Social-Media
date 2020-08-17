db792aac08ff9c3324614552923c90779de770e4
MAnish Aghi <manishaghi6@gmail.com>
2020-08-17 02:06:55 -0700

    Second Commit for Social Media

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,9 @@ const homeController = require('../controller/home_controller');
 console.log("Router Loaded");
 
 router.get('/', homeController.home);
+router.use('/users', require('./user'));
+//for any further routes, access from here
+//router.use('/routerName', require('./fileName'));
+
 
 module.exports = router;
