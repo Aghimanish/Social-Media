57254a1f8af0cf82e9069ab88a8b0048599c5fce
MAnish Aghi <manishaghi6@gmail.com>
2020-08-22 08:18:28 -0700

    Posts Added To Project

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ console.log("Router Loaded");
 
 router.get('/', homeController.home);
 router.use('/users', require('./user'));
+router.use('/posts', require('./posts'));
 //for any further routes, access from here
 //router.use('/routerName', require('./fileName'));
 
