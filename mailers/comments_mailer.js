25e8145e54f4ea497a47d57b4acf6cdbaac87925
MAnish Aghi <manishaghi6@gmail.com>
2020-09-01 05:15:51 -0700

    Parallel Jobs and Mailers Added

---
+++ mailers/comments_mailer.js
@@ -0,0 +1,18 @@
+const nodeMailer = require('../config/nodemailer');
+
+//this is another way of exporting a method
+exports.newComment = (comment) => {
+    let htmlString = nodeMailer.renderTemplate({comment:comment}, '/comments/new_comment.ejs');
+
+    nodeMailer.transporter.sendMail({
+        from:"manish@aghi.in",
+        to:comment.user.email,
+        subject: "New Comment Published",
+        html:htmlString
+    }, (err, info) => {
+        if(err){console.log("Error in sending email", err); return;}
+
+        // console.log("Message Sent", info);
+        return;
+    });
+}
