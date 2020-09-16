3929b83ec0d4592cf442843f4d6a283789e7b604
Aghimanish <manishaghi6@gmail.com>
2020-09-16 15:29:35 +0530

    Fixed Comments Delete

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -2,33 +2,31 @@ const Post = require('../models/post');
 const User = require('../models/user');
 
 module.exports.home = async function(req, res){
-    try{
-        //populate the user of each post
+    try {
         let posts = await Post.find({})
-        .sort('-createdAt')
-        .populate('user')
-        .populate({
-            path:'comments',
-            populate:{
-                path:'user'
-            },
-            populate:{
-                path:'likes'
-            }
-        }).populate('likes');
+            .sort('-createdAt')
+            .populate('user')
+            .populate({
+                path: 'comments',
+                populate: {
+                    //populate the user inside the comment
+                    path: 'user'
+                }
 
+            }).populate('likes');
+        //2.    
         let users = await User.find({});
 
-        return res.render('home',{
+        //3.
+        return res.render('home', {
             title: "Codeial | Home",
             posts: posts,
             all_users: users
         });
-    }catch(err){
-        console.log("Error", err);
+
+    } catch (error) {
+        console.log('Error', error);
         return;
-    }
-    
+    }    
 }
 
-//module.exports.actionName = function(req, res){}
